3aabb00bb18a5bc0a65e0e703c5dff5ba1601f25
rouc <72484465+rouc-tech@users.noreply.github.com>
2020-11-10 00:00:56 +0900

    fix

--- abc181_a.exs
+++ abc181_a.exs
@@ -1,21 +1,24 @@
 # https://atcoder.jp/contests/abc181/tasks/abc181_a
 # v1.10.2
 
+# 実行方法
+# echo "1243" | elixir abc181_a.exs
+
+## 答え1
 defmodule Main do
   def input do
     IO.read(:line)
     |> String.trim()
     |> String.to_integer()
   end
-
+ 
   def solve(n) do
-    m = Integer.digits(n, 2) |> List.last
-    cond do
+    case rem(n, 2) do
       0 -> "White"
       1 -> "Black"
     end
   end
-
+ 
   def main do
     input()
     |> solve()
@@ -23,6 +26,30 @@ defmodule Main do
   end
 end
 
-# 2進数に変換して、最後のビットが0or1で判断するってもあるかも
-# Integer.digits(5, 2) |> List.last
-# Integer.digits(5, 2) |> Enum.take(-1) |> List.first
+# ## 答え2
+# defmodule Main do
+#   def input do
+#     IO.read(:line)
+#       |> String.trim()
+#       |> String.last() # 最後のビット
+#       |> String.to_integer()
+#       # 2進数で取得して最後のビットが0/1で偶数or奇数を判断
+#       |> List.last
+#   end
+
+#   def solve(n) do
+#     case n do
+#       0 -> "White"
+#       1 -> "Black"
+#     end
+#   end
+
+#   def main do
+#     input()
+#       |> solve()
+#       |> IO.puts()
+#   end
+# end
+
+# コンテンスト提出時は削除
+Main.main()
